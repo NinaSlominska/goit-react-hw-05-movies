e6fe5d0ced73b0ab95404c3a2b481a127a462402
NinaSlominska <110698323+NinaSlominska@users.noreply.github.com>
2023-04-24 21:03:11 +0000

    Deploying to gh-pages from @ NinaSlominska/goit-react-hw-05-movies@ba755fd58c3cecebcfddbab4b08743217eccb857 🚀

---
+++ static/js/764.cae1ac89.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[764],{764:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:" movies"})})}}}]);
+//# sourceMappingURL=764.cae1ac89.chunk.js.map
